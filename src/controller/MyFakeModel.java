5623031bbf4520bf9cbd006bef6dff5240076a91
Petru Potrimba <petru.potrimba@studio.unibo.it>
2017-02-08 20:40:09 +0100

    Update Controller's classes

--- src/controller/MyFakeModel.java
+++ src/controller/MyFakeModel.java
@@ -1,48 +1,61 @@
 package controller;
-
 import java.util.List;
+import java.util.Map;
 
+import model.DiscountStrategy;
 import model.Lot;
 import model.ModifyList;
-import model.Pair;
 
 public interface MyFakeModel {
+
 	/**
+	 * Initializes the content of the warehouse
 	 * 
-	 * @param serializedModel content of the Filepath where the state is saved
+	 * @param serializedModel content of the file where the state is saved
 	 */
 	void initialize(String serializedModel);
 	
 	/**
+	 * Creates a serialized version of the content of the warehouse
 	 * 
 	 * @return the serialized version of the Model, to save in a file
 	 */
 	String serializeModel();
 
 	/**
+	 * Returns the content of the warehouse after modifying its content
 	 * 
 	 * @param mfl interface with only one method for either sorting the list or removing objects from it
 	 * 
-	 * @return
+	 * @return the list of lots after mfl acted upon it
 	 */
 	List<Lot> getList(ModifyList mfl);
 	
-	void addLotto(Lot lotto);
+	/**
+	 * Adds the Lot to the warehouse
+	 * @param lot new Lot to be added
+	 */
+	void addLotto(Lot lot);
 
 	/**
-	 * Removes n elements from the lotto. If the lotto has 0 items it gets removed 
-	 * @param ID
-	 * @param n
+	 * Removes n elements from the lot. If the lot has 0 items it gets removed 
+	 * @param ID id of the lot
+	 * @param n number of elements to remove from the lot
 	 */
-	void removeFromLotto(int ID, int n);
+	void removeFromLot(int ID, int n);
 	
 	/**
-	 * 
-	 * @param s strategy for deciding discount
-	 * @return
+	 * Suggests which lots should be discounted
+	 * @param ds strategy for deciding discount
+	 * @return map of lots that should be discounted, and the amount in percentage
 	 */
-	List<Pair<Lot, Integer>> getDiscountable(String s);
+	Map<Lot, Integer> getDiscountable(DiscountStrategy ds);
 	
-	void setOnSale(List<Pair<Integer, Integer>> list);
+	/**
+	 * Sets the lot with the given id to discount at the given amount
+	 * @param id the id of the lot to be discounted
+	 * @param discountAmount the discount amount in percentage
+	 */
+	void setOnSale(int id, int discountAmount);
 	
 }
