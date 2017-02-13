e4dc6110cd0ec5f354e8c362ae20fd234e8a850d
Shapour Nemati <shapournemati@gmail.com>
2017-02-13 15:03:58 +0100

    Wrote some javadoc for Lot interface

--- src/model/Lot.java
+++ src/model/Lot.java
@@ -1,29 +1,55 @@
 package model;
 
-import java.util.Date;
 import java.util.Optional;
 
+/**
+ * Represents a Lot, with the methods for getting the value of all of its fields.
+ */
 public interface Lot {
 
-        int getId(); 
-    
-	String getName();
-	
-	Optional<MyCustomDate> getExpirationDate();
-	
-	MyCustomDate getCheckInDate();
+    /**
+     * @return the id of the lot
+     */
+    int getId(); 
 
-	int getInitialQuantity();
+    /**
+     * @return name of the product
+     */
+    String getName();
 
-	int getCurrentQuantity();
+    /**
+     * @return expirationDate, which could also not be present
+     */
+    Optional<MyCustomDate> getExpirationDate();
 
-	/**
-	 * Price is in cents of euro
-	 * @return cents of euro
-	 */
-	int getPricePerSingleItem();
-	
-	boolean isOnSale();
-	
-	int getSalePercentage(); 
+    /**
+     * @return date in which the Lot was delivered to the warehouse
+     */
+    MyCustomDate getCheckInDate();
+
+    /**
+     * @return number of items initially stored in the lot
+     */
+    int getInitialQuantity();
+
+    /**
+     * @return number of items currently stored in the lot
+     */
+    int getCurrentQuantity();
+
+    /**
+     * Price is in cents of euro.
+     * @return cents of euro
+     */
+    int getPricePerSingleItem();
+
+    /**
+     * @return true if the lot is on sale, false otherwise
+     */
+    boolean isOnSale();
+
+    /**
+     * @return 0 (not on sale) - 100 (free)
+     */
+    int getSalePercentage(); 
 }
